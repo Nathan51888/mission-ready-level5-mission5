f3f36801f2a5c80e205b3922dd944f9912f31a0b
Nathan51888 <nathanliang99@outlook.com>
2024-03-28 15:27:58 +1300

    added message api

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,28 @@
-function getMessages(req, res) {
-    res.send('message');
+const mongoose = require('mongoose');
+const mongoHost = process.env.MONGO_SERVICE_HOST || 'localhost';
+const mongoPort = process.env.MONGO_SERVICE_PORT || 27017;
+
+const Message = mongoose.model('Message', { message: String });
+
+async function getMessages(req, res) {
+    await mongoose.connect(`mongodb://${mongoHost}:${mongoPort}/test`);
+
+    const foundMessage = await Message.find()
+    console.log(foundMessage);
+    res.send(foundMessage);
+
+    mongoose.connection.close();
 }
 
-module.expots = getMessages;
+async function saveMessage(req, res) {
+    await mongoose.connect(`mongodb://${mongoHost}:${mongoPort}/test`);
+
+    const newMessage = new Message({ message: 'this is a test' });
+    const message = await newMessage.save();
+    console.log('New message created');
+
+    res.send(message);
+    mongoose.connection.close();
+}
+
+module.exports = { getMessages, saveMessage };
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const controller = require('../controllers/controller.js');
+const { getMessages, saveMessage } = require('../controllers/controller.js');
 
 router.get('/', (req, res) => res.send('hello world!'));
+router.get('/get', (req, res) => getMessages(req, res));
+router.get('/send', (req, res) => saveMessage(req, res));
 
 module.exports = router;
